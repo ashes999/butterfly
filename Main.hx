b2b630d46a63f2320f4120c5ca08f5433f5bad64
ashes999 <ashes999@yahoo.com>
2015-11-26 23:04:16 -0500

    Error checking for posts dir; refactor error function name

--- Main.hx
+++ Main.hx
@@ -9,17 +9,14 @@ class Main {
 
   public function run() : Void {
     if (Sys.args().length != 1) {
-      errorOut("Usage: neko Main.n <source directory>");
+      errorAndExit("Usage: neko Main.n <source directory>");
     }
 
     var srcDir = Sys.args()[0];
     trace("Using " + srcDir + " as source directory ...");
 
-    if (!sys.FileSystem.exists(srcDir)) {
-      errorOut(srcDir + " doesn't exist");
-    }
-    if (!sys.FileSystem.isDirectory(srcDir)) {
-      errorOut(srcDir + " isn't a directory");
+    if (!sys.FileSystem.exists(srcDir) || !sys.FileSystem.isDirectory(srcDir)) {
+      errorAndExit(srcDir + " doesn't exist or isn't a directory");
     }
 
     var binDir = srcDir + "/bin";
@@ -27,9 +24,18 @@ class Main {
       sys.FileSystem.createDirectory(binDir);
       trace("Created " + binDir);
     }
+
+    if (!sys.FileSystem.exists(srcDir + "/src/layout.html")) {
+      errorAndExit("Can't find " + srcDir + "/src/layout.html");
+    }
+
+    var postsDir = srcDir + '/posts';
+    if (!sys.FileSystem.exists(postsDir) || !sys.FileSystem.isDirectory(postsDir)) {
+      errorAndExit(postsDir + " doesn't exist or isn't a directory");
+    }
   }
 
-  private function errorOut(message:String) : Void {
+  private function errorAndExit(message:String) : Void {
     trace("Error: " + message);
     Sys.exit(1);
   }
