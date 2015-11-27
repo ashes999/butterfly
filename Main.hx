1f9ba6ddae27e04987dc5cda3ce53d0dddb9a95a
ashes999 <ashes999@yahoo.com>
2015-11-26 22:52:08 -0500

    From static to instance; error-handling separated; create bin dir

--- Main.hx
+++ Main.hx
@@ -1,9 +1,36 @@
 class Main {
-  static public function main():Void {
+  static public function main() : Void {
+    new Main().run();
+  }
+
+  public function new() {
+
+  }
+
+  public function run() : Void {
     if (Sys.args().length != 1) {
-      trace("Usage: neko Main.n <source directory>");
-    } else {
-      trace("Using " + Sys.args()[0] + " as source directory ...");
+      errorOut("Usage: neko Main.n <source directory>");
     }
+
+    var srcDir = Sys.args()[0];
+    trace("Using " + srcDir + " as source directory ...");
+
+    if (!sys.FileSystem.exists(srcDir)) {
+      errorOut(srcDir + " doesn't exist");
+    }
+    if (!sys.FileSystem.isDirectory(srcDir)) {
+      errorOut(srcDir + " isn't a directory");
+    }
+
+    var binDir = srcDir + "/bin";
+    if (!sys.FileSystem.exists(binDir)) {
+      sys.FileSystem.createDirectory(binDir);
+      trace("Created " + binDir);
+    }
+  }
+
+  private function errorOut(message:String) : Void {
+    trace("Error: " + message);
+    Sys.exit(1);
   }
 }
