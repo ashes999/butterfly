84b2449d089c0eeaf00687d282d2a8a7e5fecf5b
ashes999 <ashes999@yahoo.com>
2016-02-14 00:34:31 -0500

    Refactoring: move file system stuff to FileSystem class

--- src/Main.hx
+++ src/Main.hx
@@ -6,6 +6,7 @@ import butterfly.generator.AtomGenerator;
 import butterfly.generator.HtmlGenerator;
 import butterfly.html.FileWriter;
 import butterfly.html.LayoutModifier;
+import butterfly.io.FileSystem;
 
 class Main {
   static public function main() : Void {
@@ -21,17 +22,17 @@ class Main {
 
     var projectDir = Sys.args()[0];
     trace("Using " + projectDir + " as project directory");
-    ensureDirExists(projectDir);
+    FileSystem.ensureDirExists(projectDir);
 
     var binDir = projectDir + "/bin";
     if (sys.FileSystem.exists(binDir)) {
       // always clean/rebuild
-      deleteDirRecursively(binDir);
+      FileSystem.deleteDirRecursively(binDir);
       sys.FileSystem.createDirectory(binDir);
     }
 
     var srcDir = projectDir + "/src";
-    ensureDirExists(srcDir);
+    FileSystem.ensureDirExists(srcDir);
 
     var configFile = '${srcDir}/config.json';
     if (!sys.FileSystem.exists(configFile)) {
@@ -39,7 +40,7 @@ class Main {
     }
     var config:Dynamic = haxe.Json.parse(sys.io.File.getContent(configFile));
 
-    copyDirRecursively('${srcDir}/content', '${binDir}/content');
+    FileSystem.copyDirRecursively('${srcDir}/content', '${binDir}/content');
 
     var layoutFile = srcDir + "/layout.html";
 
@@ -98,53 +99,6 @@ class Main {
     writer.write("atom.xml", atomXml);
 
     trace('Generated index page, ${pages.length} page(s), and ${posts.length} post(s).');
-  }
-
-  private function ensureDirExists(path:String) : Void
-  {
-    if (!sys.FileSystem.exists(path)) {
-      throw path + " doesn't exist";
-    } else if (!sys.FileSystem.isDirectory(path)) {
-      throw path + " isn't a directory";
-    }
-  }
-
-  private function copyDirRecursively(srcPath:String, destPath:String) : Void
-  {
-    if (!sys.FileSystem.exists(destPath)) {
-      sys.FileSystem.createDirectory(destPath);
-    }
-
-    if (sys.FileSystem.exists(srcPath) && sys.FileSystem.isDirectory(srcPath))
-    {
-      var entries = sys.FileSystem.readDirectory(srcPath);
-      for (entry in entries) {
-        if (sys.FileSystem.isDirectory(srcPath + '/' + entry)) {
-          sys.FileSystem.createDirectory(srcPath + '/' + entry);
-          copyDirRecursively(srcPath + '/' + entry, destPath + '/' + entry);
-        } else {
-          sys.io.File.copy(srcPath + '/' + entry, destPath + '/' + entry);
-        }
-      }
-    } else {
-      throw srcPath + " doesn't exist or isn't a directory";
-    }
-  }
-
-  private function deleteDirRecursively(path:String) : Void
-  {
-    if (sys.FileSystem.exists(path) && sys.FileSystem.isDirectory(path))
-    {
-      var entries = sys.FileSystem.readDirectory(path);
-      for (entry in entries) {
-        if (sys.FileSystem.isDirectory(path + '/' + entry)) {
-          deleteDirRecursively(path + '/' + entry);
-          sys.FileSystem.deleteDirectory(path + '/' + entry);
-        } else {
-          sys.FileSystem.deleteFile(path + '/' + entry);
-        }
-      }
-    }
   }
 
   private function getPostsOrPages(path:String, ?isPage:Bool = false) : Array<Post>
---
+++ src/butterfly/io/FileSystem.hx
@@ -0,0 +1,52 @@
+package butterfly.io;
+
+// static class
+class FileSystem
+{
+  public static function copyDirRecursively(srcPath:String, destPath:String) : Void
+  {
+    if (!sys.FileSystem.exists(destPath)) {
+      sys.FileSystem.createDirectory(destPath);
+    }
+
+    if (sys.FileSystem.exists(srcPath) && sys.FileSystem.isDirectory(srcPath))
+    {
+      var entries = sys.FileSystem.readDirectory(srcPath);
+      for (entry in entries) {
+        if (sys.FileSystem.isDirectory(srcPath + '/' + entry)) {
+          sys.FileSystem.createDirectory(srcPath + '/' + entry);
+          copyDirRecursively(srcPath + '/' + entry, destPath + '/' + entry);
+        } else {
+          sys.io.File.copy(srcPath + '/' + entry, destPath + '/' + entry);
+        }
+      }
+    } else {
+      throw srcPath + " doesn't exist or isn't a directory";
+    }
+  }
+
+  public static function deleteDirRecursively(path:String) : Void
+  {
+    if (sys.FileSystem.exists(path) && sys.FileSystem.isDirectory(path))
+    {
+      var entries = sys.FileSystem.readDirectory(path);
+      for (entry in entries) {
+        if (sys.FileSystem.isDirectory(path + '/' + entry)) {
+          deleteDirRecursively(path + '/' + entry);
+          sys.FileSystem.deleteDirectory(path + '/' + entry);
+        } else {
+          sys.FileSystem.deleteFile(path + '/' + entry);
+        }
+      }
+    }
+  }
+
+  public static function ensureDirExists(path:String) : Void
+  {
+    if (!sys.FileSystem.exists(path)) {
+      throw path + " doesn't exist";
+    } else if (!sys.FileSystem.isDirectory(path)) {
+      throw path + " isn't a directory";
+    }
+  }
+}
